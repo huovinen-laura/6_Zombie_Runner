61e32f722dfc52f14971453f2d92b3822f162a28
Laura Huovinen <laura.huovinen@outlook.com>
2022-12-12 12:10:46 +0200

    Added a death handler
    - Working basic game loop

--- 6_Zombie_Runner/Assets/Scripts/PlayerHealth.cs
+++ 6_Zombie_Runner/Assets/Scripts/PlayerHealth.cs
@@ -12,7 +12,7 @@ public class PlayerHealth : MonoBehaviour
 
         if (hitPoints <= 0)
         {
-            Debug.Log("Bam, you're dead");
+            GetComponent<DeathHandler>().HandleDeath();
         }
     }
 }
